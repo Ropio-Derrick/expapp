a5f09b0182e666a7d72d8ec15f723e049b196ec9
Ropio-Derrick <rodetuk@gmail.com>
2022-10-24 11:12:20 +0300

    Pushing what ihave

--- App.js
+++ App.js
@@ -4,8 +4,6 @@ import { Button, StyleSheet, Text, View } from "react-native";
 
 const App = () => {
   const [name, setName] = useState("mash");
-  const [session, setSession] = useState({ number: 6, title: "state" });
-  const [current, setCurrent] = useState(true);
 
   const onClickHandler = () => {
     setName("Programming with mash");
@@ -13,30 +11,26 @@ const App = () => {
     setCurrent(false);
   };
   return (
-    <View style={styles.container}>
+    <View style={styles.body}>
+      <StatusBar style="auto" />
       <Text style={styles.text}> {name}</Text>
-      <Text style={styles.text}>
-        This is session Number {session.number} and about {session.title}
-      </Text>
-      <Text style={styles.text}>
-        {" "}
-        {current ? "current session" : "next session"}
-      </Text>
 
       <Button title="Update State" onPress={onClickHandler}>
         {" "}
       </Button>
-      <StatusBar style="auto" />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "black",
+  body: {
+    width: "100%",
+    height: "50%",
+    backgroundColor: "#de8727",
     alignItems: "center",
     justifyContent: "center",
+    borderWidth: "10",
+    borderColor: "red",
   },
   text: {
     textAlign: "center",
